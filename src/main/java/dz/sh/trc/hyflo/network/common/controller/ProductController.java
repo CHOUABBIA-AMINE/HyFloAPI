0abee83fcc8dffab2d875034eb601de908075e67
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2026-01-17 02:01:30 +0100

    Common Sources

--- src/main/java/dz/sh/trc/hyflo/network/common/controller/ProductController.java
+++ src/main/java/dz/sh/trc/hyflo/network/common/controller/ProductController.java
@@ -14,17 +14,23 @@
 
 package dz.sh.trc.hyflo.network.common.controller;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import dz.sh.trc.hyflo.configuration.template.GenericController;
 import dz.sh.trc.hyflo.network.common.dto.ProductDTO;
 import dz.sh.trc.hyflo.network.common.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/network/common/product")
@@ -88,6 +94,11 @@ public class ProductController extends GenericController<ProductDTO, Long> {
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "asc") String sortDir) {
         return super.search(q, page, size, sortBy, sortDir);
+    }
+
+    @Override
+    protected Page<ProductDTO> searchByQuery(String query, Pageable pageable) {
+        return productService.globalSearch(query, pageable);
     }
 
     @Override
