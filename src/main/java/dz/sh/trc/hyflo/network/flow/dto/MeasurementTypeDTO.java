8a0f0f7efb62a305808c6573d86aec2461ee7897
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-20 13:21:32 +0100

    Add DTOs for MeasurementType, ValidationStatus, QualityFlag, DataSource

---
+++ src/main/java/dz/sh/trc/hyflo/network/flow/dto/MeasurementTypeDTO.java
@@ -0,0 +1,81 @@
+/**
+ *	
+ *	@Author		: CHOUABBIA-AMINE
+ *
+ *	@Name		: MeasurementTypeDTO
+ *	@CreatedOn	: 01-20-2026
+ *	@UpdatedOn	: 01-20-2026
+ *
+ *	@Type		: Class
+ *	@Layer		: DTO
+ *	@Package	: Network / Flow
+ *
+ **/
+
+package dz.sh.trc.hyflo.network.flow.dto;
+
+import dz.sh.trc.hyflo.configuration.template.GenericDTO;
+import dz.sh.trc.hyflo.network.flow.model.MeasurementType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MeasurementTypeDTO extends GenericDTO<MeasurementType> {
+    
+    @NotBlank(message = "Code is required")
+    private String code;
+    
+    private String designationAr;
+    private String designationEn;
+    
+    @NotBlank(message = "French designation is required")
+    private String designationFr;
+    
+    private String descriptionAr;
+    private String descriptionEn;
+    private String descriptionFr;
+    
+    @Override
+    public MeasurementType toEntity() {
+        MeasurementType entity = new MeasurementType();
+        entity.setId(getId());
+        entity.setCode(this.code);
+        entity.setDesignationAr(this.designationAr);
+        entity.setDesignationEn(this.designationEn);
+        entity.setDesignationFr(this.designationFr);
+        entity.setDescriptionAr(this.descriptionAr);
+        entity.setDescriptionEn(this.descriptionEn);
+        entity.setDescriptionFr(this.descriptionFr);
+        return entity;
+    }
+    
+    @Override
+    public void updateEntity(MeasurementType entity) {
+        if (this.code != null) entity.setCode(this.code);
+        if (this.designationAr != null) entity.setDesignationAr(this.designationAr);
+        if (this.designationEn != null) entity.setDesignationEn(this.designationEn);
+        if (this.designationFr != null) entity.setDesignationFr(this.designationFr);
+        if (this.descriptionAr != null) entity.setDescriptionAr(this.descriptionAr);
+        if (this.descriptionEn != null) entity.setDescriptionEn(this.descriptionEn);
+        if (this.descriptionFr != null) entity.setDescriptionFr(this.descriptionFr);
+    }
+    
+    public static MeasurementTypeDTO fromEntity(MeasurementType entity) {
+        if (entity == null) return null;
+        return MeasurementTypeDTO.builder()
+                .id(entity.getId())
+                .code(entity.getCode())
+                .designationAr(entity.getDesignationAr())
+                .designationEn(entity.getDesignationEn())
+                .designationFr(entity.getDesignationFr())
+                .descriptionAr(entity.getDescriptionAr())
+                .descriptionEn(entity.getDescriptionEn())
+                .descriptionFr(entity.getDescriptionFr())
+                .build();
+    }
+}
