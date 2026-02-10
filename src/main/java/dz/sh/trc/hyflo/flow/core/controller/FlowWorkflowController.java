23357610e43231b36e2c15bc9151c12225c840e3
Amine CHOUABBIA <chouabbia.amine@gmail.com>
2026-02-10 14:23:41 +0100

    Repository to analyze

--- src/main/java/dz/sh/trc/hyflo/flow/core/controller/FlowWorkflowController.java
+++ src/main/java/dz/sh/trc/hyflo/flow/core/controller/FlowWorkflowController.java
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dz.sh.trc.hyflo.flow.core.dto.command.ReadingSubmitRequestDTO;
 import dz.sh.trc.hyflo.flow.core.dto.command.ReadingValidationRequestDTO;
-import dz.sh.trc.hyflo.flow.core.dto.command.SlotCoverageRequestDTO;
-import dz.sh.trc.hyflo.flow.core.dto.command.SlotCoverageResponseDTO;
 import dz.sh.trc.hyflo.flow.core.service.FlowReadingWorkflowService;
+import dz.sh.trc.hyflo.flow.intelligence.dto.monitoring.SlotCoverageRequestDTO;
+import dz.sh.trc.hyflo.flow.intelligence.dto.monitoring.SlotCoverageResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
