124399d4afc4a0d2bb3b8e5a26d97bc893c2cb77
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2026-02-14 20:39:24 +0100

    update flowIntelligenceController

--- src/main/java/dz/sh/trc/hyflo/flow/intelligence/controller/PipelineIntelligenceController.java
+++ src/main/java/dz/sh/trc/hyflo/flow/intelligence/controller/PipelineIntelligenceController.java
@@ -20,17 +20,16 @@ import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import dz.sh.trc.hyflo.flow.intelligence.dto.PipelineInfoDTO;
 import dz.sh.trc.hyflo.flow.intelligence.dto.PipelineDynamicDashboardDTO;
-import dz.sh.trc.hyflo.flow.intelligence.dto.PipelineTimelineDTO;
+import dz.sh.trc.hyflo.flow.intelligence.dto.PipelineInfoDTO;
 import dz.sh.trc.hyflo.flow.intelligence.dto.PipelineOverviewDTO;
+import dz.sh.trc.hyflo.flow.intelligence.dto.PipelineTimelineDTO;
 import dz.sh.trc.hyflo.flow.intelligence.dto.ReadingsTimeSeriesDTO;
 import dz.sh.trc.hyflo.flow.intelligence.dto.SlotStatusDTO;
 import dz.sh.trc.hyflo.flow.intelligence.service.PipelineIntelligenceService;
