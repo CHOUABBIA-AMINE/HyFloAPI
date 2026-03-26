02db0dab9995e31953d7cb7f8829ed3dddd9041f
Amine CHOUABBIA <chouabbia.amine@gmail.com>
2026-03-26 12:17:37 +0100

    update imports at FlowEventQueryService and FlowForecastQueryService

--- src/main/java/dz/sh/trc/hyflo/flow/core/service/FlowForecastQueryService.java
+++ src/main/java/dz/sh/trc/hyflo/flow/core/service/FlowForecastQueryService.java
@@ -22,9 +22,7 @@ import dz.sh.trc.hyflo.flow.core.repository.FlowForecastRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
